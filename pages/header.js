3d6333a2eb0be12eeeda9ef4695699e4b8759c48
Madison W. Call <madison.call122@gmail.com>
2025-01-28 20:12:34 -0800

    Added elements. Need to figure out how to use bootstrap styling

--- pages/header.js
+++ pages/header.js
@@ -1,12 +1,21 @@
 import Container from 'react-bootstrap/Container';
 import NavBar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 export default function Header() {
     return (
       <div> 
-        <NavBar variant="dark" expand="lg" className="bg-body-tertiary" bg="dark" data-bs-theme="dark">
+        <NavBar expand="lg" className="bg-body-tertiary" bg="dark" data-bs-theme="dark">
             <Container>
-                <NavBar.Brand href="#home"> This is my header </NavBar.Brand>
+                <NavBar.Brand href="#home"> This is my header image </NavBar.Brand>
+                <NavBar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="home">Home</Nav.Link>
+                        <Nav.Link href="about">About</Nav.Link>
+                        <Nav.Link href="projects">Projects</Nav.Link>
+                        <Nav.Link href="papers">Papers</Nav.Link>
+                    </Nav>
+                </NavBar.Collapse>
             </Container>
         </NavBar>
       </div>
